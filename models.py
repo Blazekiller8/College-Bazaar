4ed0d671d3275722a51e04a9e4757621b13cb2f7
Blazekiller8 <sivaraamtk@hotmail.com>
2022-08-29 22:16:10 +0530

    created admin and product post CRUD operations

--- models.py
+++ models.py
@@ -36,3 +36,48 @@ class StudentModel(BaseModel):
                 "password": "secret"
             }
         }
+
+class AdminModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+                "password": "secret"
+            }
+        }
+
+class ProductModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    seller_email: EmailStr = Field(...)
+    description: str = Field(...)
+    price: float = Field(...)
+    image_url: str = Field(...)
+    category: str = Field(...)
+    tags: list = Field(...)
+
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                'name': "HAWK NUAGE 27T",
+                'seller_email': "jdoe@example.com",
+                'description': "2 months old Bicycle in perfect condition",
+                'price': 4000.00,
+                'image_url': "https://www.herocycles.com/admin/public/uploads/products/60c8a7bb2b97dg4viE4RhYq.png",
+                'category': "Bicycles",
+                'tags': ['bicycle','hero cycles','hawk nuage']
+            }
+        }
